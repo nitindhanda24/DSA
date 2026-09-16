1d51256f90f4664100bd9f61281267e8478d4f2b
nitindhanda24 <nitindhanda72@gmail.com>
2026-09-16 17:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (29.15%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+        if(s.length()!=t.length()) return false;
+
+        int n=s.length();
+
+        
+        map<char,int>mp1;
+         map<char,int>mp2;
+
+        for(int i=0;i<n;i++){
+
+            if(mp1.find(s[i])==mp1.end()){
+                mp1[s[i]]=i;
+            }
+             if(mp2.find(t[i])==mp2.end()){
+                mp2[t[i]]=i;
+            }
+
+            if(mp1[s[i]]!=mp2[t[i]]){
+                return false;
+            }
+
+           
+        }
+
+     return true;
+
+        
+    }
+};
